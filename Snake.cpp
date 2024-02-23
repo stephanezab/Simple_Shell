147e5a19be6eee9d6e7947a47f112eb70033a249
stephanezab <stephanezab@gmail.com>
2024-02-22 23:18:14 -0500

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-//#include <raylib.h>
+#include <raylib.h>
 
 using namespace std;
 
 int main () {
-
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    
     return 0;
 }
