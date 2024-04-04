7daab3e272e755673301ceb3f33479a1811fa12b
stephanezab <stephanezab@gmail.com>
2024-04-04 13:09:04 -0400

    Update Shell.cpp

--- Shell.cpp
+++ Shell.cpp
@@ -115,6 +115,7 @@ void help() {
               << "cd [directory]     - Move to the desired directory\n"
               << "mkdir [directory]  - Create the desired directory\n"
               << "rm [file]          - Remove file from directory\n"
+              << "rm -r [file]       - Remove directory\n"
               << "exit               - Exit the CLI\n";
 
 }
