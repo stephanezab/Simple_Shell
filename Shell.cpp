71802aff50dfc39c25fb8fd3c2abce5608fa6bdc
stephanezab <stephanezab@gmail.com>
2024-04-08 10:52:32 -0400

    Update Shell.cpp

--- Shell.cpp
+++ Shell.cpp
@@ -25,6 +25,7 @@ void CD(const std::string& input);
 void cat(const std::string& input);
 void MK(const std::string& input);
 void rm(const std::string& input);
+void execpp(const std::string& input);
 
 // Command map associating command strings with their corresponding functions
 std::map<std::string, void (*)(const std::string&)> commands = {
@@ -38,7 +39,8 @@ std::map<std::string, void (*)(const std::string&)> commands = {
     {"cd", CD},
     {"cat", cat},
     {"mkdir", MK},
-    {"rm", rm}
+    {"rm", rm},
+    {"g++", execpp}
 
     // Add more commands here
 };
@@ -290,5 +292,35 @@ void rm(const std::string& input){
         if (WIFEXITED(status)) {
             std::cout << "Child process exited with code " << WEXITSTATUS(status) << std::endl;
         }
+    }
+}
+
+void execpp(const std::string& input){
+
+    size_t s = input.find(".");
+    std::string file = input.substr(0,s);
+    
+    std::vector<const char*> command = {"g++", input.c_str(),"-o", file.c_str(), nullptr};
+
+    pid_t pid = fork(); // Create a new process
+
+    if (pid == -1) {
+        // If fork() returns -1, an error occurred
+        std::cerr << "Fork failed" << std::endl;
+        exit(1);
+    } else if (pid == 0) {
+        // Child process
+        // Execute the command using execvp
+        execvp(command[0], const_cast<char* const*>(command.data()));
+
+        // If execvp returns, it means an error occurred
+        std::cerr << "Error executing execvp" << std::endl;
+        exit(EXIT_FAILURE); // Exit the child process with an error code
+    } else {
+        // Parent process
+        int status;
+        waitpid(pid, &status, 0); // Wait for the child process to finish
+
+        
     }
 }
