273b3a744d663e657dc6b497fc4c0b1d821ff519
stephanezab <stephanezab@gmail.com>
2024-04-22 18:59:44 -0400

    Update Shell.cpp

--- Shell.cpp
+++ Shell.cpp
@@ -13,28 +13,28 @@
 using namespace std;
 
 // Function prototypes
-void execute_command(const std::string& inputLine);
+void execute_command(const string& inputLine);
 void help();
-void echo(const std::string& input);
+void echo(const string& input);
 void exitCLI();
 void date();
 void PWD();
 void clear();
-void LS(const std::string &input);
-void CD(const std::string& input);
-void cat(const std::string& input);
-void MK(const std::string& input);
-void rm(const std::string& input);
-void execpp(const std::string& input);
+void LS(const string &input);
+void CD(const string& input);
+void cat(const string& input);
+void MK(const string& input);
+void rm(const string& input);
+void execpp(const string& input);
 
 // Command map associating command strings with their corresponding functions
-std::map<std::string, void (*)(const std::string&)> commands = {
-    {"help", [](const std::string&){ help(); }},
+map<string, void (*)(const string&)> commands = {
+    {"help", [](const string&){ help(); }},
     {"echo", echo},
-    {"exit", [](const std::string&){ exitCLI(); }},
-    {"date", [](const std::string&){ date(); }},
-    {"pwd", [](const std::string&){ PWD(); }},
-    {"clear", [](const std::string&){ clear(); }},
+    {"exit", [](const string&){ exitCLI(); }},
+    {"date", [](const string&){ date(); }},
+    {"pwd", [](const string&){ PWD(); }},
+    {"clear", [](const string&){ clear(); }},
     {"ls", LS},
     {"cd", CD},
     {"cat", cat},
@@ -49,14 +49,14 @@ int main() {
 
     system("clear"); // to clear the main shell 
 
-    std::string inputLine;
-    std::cout << "Simple CLI. Type 'help' for a list of commands.\n";
+    string inputLine;
+    cout << "Simple CLI. Type 'help' for a list of commands.\n";
    
     
     while (true) {
-        std::filesystem::path cwd = std::filesystem::current_path();
-        std::cout << ">> ";
-        std::getline(std::cin, inputLine);
+        filesystem::path cwd = filesystem::current_path();
+        cout << ">> ";
+        getline(cin, inputLine);
 
         
         // Find the first space to separate the command from its arguments
@@ -68,15 +68,15 @@ int main() {
     return 0;
 }
 
-void execute_command(const std::string& inputLine){
+void execute_command(const string& inputLine){
     size_t index = inputLine.find("&&");
 
 
-    if (index == std::string::npos)
+    if (index == string::npos)
     {
         size_t spaceIndex = inputLine.find(' ');
-        std::string command = inputLine.substr(0, spaceIndex);
-        std::string argument = spaceIndex != std::string::npos ? inputLine.substr(spaceIndex + 1) : "";
+        string command = inputLine.substr(0, spaceIndex);
+        string argument = spaceIndex != string::npos ? inputLine.substr(spaceIndex + 1) : "";
 
         // Execute the command if it is found
         auto commandIt = commands.find(command);
@@ -87,12 +87,12 @@ void execute_command(const std::string& inputLine){
         }
         else
         {
-            std::cout << "Unknown command. Type 'help' for a list of commands.\n";
+            cout << "Unknown command. Type 'help' for a list of commands.\n";
         }
     }
-    else if (index != std::string::npos){ // fix this today
-        std::string line1 = inputLine.substr(0, index - 1);
-        std::string line2 = inputLine.substr(index + 3);
+    else if (index != string::npos){ // fix this today
+        string line1 = inputLine.substr(0, index - 1);
+        string line2 = inputLine.substr(index + 3);
 
         execute_command(line1);
         execute_command(line2);
@@ -104,7 +104,7 @@ void execute_command(const std::string& inputLine){
 }
 
 void help() {
-    std::cout << "Available commands:\n"
+    cout << "Available commands:\n"
               << "help               - Show this help message\n"
               << "echo [text]        - Echo the input text\n"
               << "date               - Show the current working directory\n"
@@ -120,12 +120,12 @@ void help() {
 
 }
 
-void echo(const std::string& input) {
-    std::cout << input << std::endl;
+void echo(const string& input) {
+    cout << input << endl;
 }
 
 void exitCLI() {
-    std::cout << "Exiting CLI.\n";
+    cout << "Exiting CLI.\n";
     exit(0);
 }
 
@@ -144,15 +144,15 @@ void clear(){
 // void LS(){
 //     DIR *dp;    // holds current directory
 //     dirent *d;  // holds the directory and file info inside current directory 
-//     std::filesystem::path cwd = std::filesystem::current_path();
-//     std::string cwdString = cwd.string(); // Convert path to string
+//     filesystem::path cwd = filesystem::current_path();
+//     string cwdString = cwd.string(); // Convert path to string
 
 
 //     if(dp = opendir(cwdString.c_str())){
 //         // read directory dp and send info to d
 //         while(d = readdir(dp)){
 //             if (d->d_name[0] != '.' ){
-//                 std::cout<<d->d_name<<"\n";
+//                 cout<<d->d_name<<"\n";
 //             } 
             
 //         }
@@ -160,14 +160,14 @@ void clear(){
 
 // }
 
-void LS(const std::string &input){
+void LS(const string &input){
     
 
     pid_t pid = fork(); // Create a new process
 
     if (pid == -1) {
         // If fork() returns -1, an error occurred
-        std::cerr << "Fork failed" << std::endl;
+        cerr << "Fork failed" << endl;
         exit(1);
     } else if (pid == 0) {
         // Child process
@@ -181,7 +181,7 @@ void LS(const std::string &input){
             execvp("ls", const_cast<char* const*>(argv));
         }
         // If execvp returns, it means an error occurred
-        std::cerr << "Error executing execvp" << std::endl;
+        cerr << "Error executing execvp" << endl;
         exit(EXIT_FAILURE); // Exit the child process with an error code
     } else {
         // Parent process
@@ -189,25 +189,25 @@ void LS(const std::string &input){
         waitpid(pid, &status, 0); // Wait for the child process to finish
 
         // if (WIFEXITED(status)) {
-        //     std::cout << "Child process exited with code " << WEXITSTATUS(status) << std::endl;
+        //     cout << "Child process exited with code " << WEXITSTATUS(status) << endl;
         // }
     }
 }
 
-void CD(const std::string& input){
+void CD(const string& input){
     
     chdir(input.c_str());
     
 }
 
-void cat(const std::string& input){
-    std::vector<const char*> command = {"cat", input.c_str(), nullptr};
+void cat(const string& input){
+    vector<const char*> command = {"cat", input.c_str(), nullptr};
 
     pid_t pid = fork(); // Create a new process
 
     if (pid == -1) {
         // If fork() returns -1, an error occurred
-        std::cerr << "Fork failed" << std::endl;
+        cerr << "Fork failed" << endl;
         exit(1);
     } else if (pid == 0) {
         // Child process
@@ -215,7 +215,7 @@ void cat(const std::string& input){
         execvp(command[0], const_cast<char* const*>(command.data()));
 
         // If execvp returns, it means an error occurred
-        std::cerr << "Error executing execvp" << std::endl;
+        cerr << "Error executing execvp" << endl;
         exit(EXIT_FAILURE); // Exit the child process with an error code
     } else {
         // Parent process
@@ -225,14 +225,14 @@ void cat(const std::string& input){
     }
 }
 
-void MK(const std::string& input){
-    std::vector<const char*> command = {"mkdir", input.c_str(), nullptr};
+void MK(const string& input){
+    vector<const char*> command = {"mkdir", input.c_str(), nullptr};
 
     pid_t pid = fork(); // Create a new process
 
     if (pid == -1) {
         // If fork() returns -1, an error occurred
-        std::cerr << "Fork failed" << std::endl;
+        cerr << "Fork failed" << endl;
         exit(1);
     } else if (pid == 0) {
         // Child process
@@ -240,7 +240,7 @@ void MK(const std::string& input){
         execvp(command[0], const_cast<char* const*>(command.data()));
 
         // If execvp returns, it means an error occurred
-        std::cerr << "Error executing execvp" << std::endl;
+        cerr << "Error executing execvp" << endl;
         exit(EXIT_FAILURE); // Exit the child process with an error code
     } else {
         // Parent process
@@ -250,7 +250,7 @@ void MK(const std::string& input){
     }
 }
 
-void rm(const std::string& input){ 
+void rm(const string& input){ 
     size_t f = input.find("-r");
     
 
@@ -258,25 +258,25 @@ void rm(const std::string& input){
 
     if (pid == -1) {
         // If fork() returns -1, an error occurred
-        std::cerr << "Fork failed" << std::endl;
+        cerr << "Fork failed" << endl;
         exit(1);
     } else if (pid == 0) {
         // Child process
         // Execute the command using execvp
-        if (f == std::string::npos){
-            std::vector<const char*> command = {"rm", input.c_str(), nullptr};
+        if (f == string::npos){
+            vector<const char*> command = {"rm", input.c_str(), nullptr};
             execvp(command[0], const_cast<char* const*>(command.data()));
         }
         else{
-            std::string com2 = input.substr(f,f+2);
-            std::string arg = input.substr(f+3);
-            std::vector<const char*> command = {"rm", com2.c_str(), arg.c_str(), nullptr};
+            string com2 = input.substr(f,f+2);
+            string arg = input.substr(f+3);
+            vector<const char*> command = {"rm", com2.c_str(), arg.c_str(), nullptr};
             execvp(command[0], const_cast<char* const*>(command.data()));
 
         }     
 
         // If execvp returns, it means an error occurred
-        std::cerr << "Error executing execvp" << std::endl;
+        cerr << "Error executing execvp" << endl;
         exit(EXIT_FAILURE); // Exit the child process with an error code
     } else {
         // Parent process
@@ -286,18 +286,18 @@ void rm(const std::string& input){
     }
 }
 
-void execpp(const std::string& input){
+void execpp(const string& input){
 
     size_t s = input.find(".");
-    std::string file = input.substr(0,s);
+    string file = input.substr(0,s);
 
-    std::vector<const char*> command = {"g++", input.c_str(),"-o", file.c_str(), nullptr};
+    vector<const char*> command = {"g++", input.c_str(),"-o", file.c_str(), nullptr};
 
     pid_t pid = fork(); // Create a new process
 
     if (pid == -1) {
         // If fork() returns -1, an error occurred
-        std::cerr << "Fork failed" << std::endl;
+        cerr << "Fork failed" << endl;
         exit(1);
     } else if (pid == 0) {
         // Child process
@@ -305,29 +305,29 @@ void execpp(const std::string& input){
         execvp(command[0], const_cast<char* const*>(command.data()));
 
         // If execvp returns, it means an error occurred
-        std::cerr << "Error executing execvp" << std::endl;
+        cerr << "Error executing execvp" << endl;
         exit(EXIT_FAILURE); // Exit the child process with an error code
     } else {
         // Parent process
         int status;
         waitpid(pid, &status, 0); // Wait for the child process to finish
 
-        std::string slash = "./";
-        std::string execfile =  slash + file;
+        string slash = "./";
+        string execfile =  slash + file;
 
-        std::vector<const char*> command2 = {execfile.c_str(), nullptr};
+        vector<const char*> command2 = {execfile.c_str(), nullptr};
 
         pid_t pid2 = fork();
 
         if(pid2 == -1){
-            std::cerr << "Second Fork failed" << std::endl;
+            cerr << "Second Fork failed" << endl;
             exit(1);
 
         }else if(pid2 == 0){
             
             execvp(command2[0], const_cast<char* const*>(command2.data()));
 
-             std::cerr << "Error executing second execvp" << std::endl;
+             cerr << "Error executing second execvp" << endl;
              exit(EXIT_FAILURE); 
         }else{
             int status2;
